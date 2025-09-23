eee3b2f00051d81b68fe5bb36038ce5733ff4852
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2025-09-23 23:19:43 +0200

    upgrade tanstack

--- src/entry-client.tsx
+++
@@ -1,7 +0,0 @@
-import { StartClient } from "@tanstack/react-start/client";
-import { hydrateRoot } from "react-dom/client";
-import { createRouter } from "./router";
-
-const router = createRouter();
-
-hydrateRoot(document, <StartClient router={router} />);
