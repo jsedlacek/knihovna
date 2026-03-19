ad447fc90a00c763fc391937650daa782b6585de
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2026-03-19 19:58:15 +0100

    Revert "Fix Worker crash: default to plain pino (no pino-pretty transport)"
    This reverts commit cd26f53fb844bde5f93dc41986da825cd7134e70.

--- src/lib/server/utils/logger.ts
+++ src/lib/server/utils/logger.ts
@@ -1,6 +1,6 @@
 import pino from "pino";
 
-const isDev = process.env["NODE_ENV"] === "development";
+const isDev = process.env["NODE_ENV"] !== "production";
 
 export const logger = pino({
   level: process.env["LOG_LEVEL"] ?? "info",
