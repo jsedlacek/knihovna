5de5d4ffc15024eaddaca2ab13bf788ea1884167
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2026-03-19 19:59:11 +0100

    Import pino pretty

--- src/lib/server/utils/logger.ts
+++ src/lib/server/utils/logger.ts
@@ -1,15 +1,11 @@
 import pino from "pino";
+import pretty from "pino-pretty";
 
 const isDev = process.env["NODE_ENV"] !== "production";
+const level = process.env["LOG_LEVEL"] ?? "info";
+const stream = isDev ? pretty() : undefined;
 
-export const logger = pino({
-  level: process.env["LOG_LEVEL"] ?? "info",
-  ...(isDev && {
-    transport: {
-      target: "pino-pretty",
-    },
-  }),
-});
+export const logger = pino({ level }, stream);
 
 export function createLogger(component: string) {
   return logger.child({ component });
