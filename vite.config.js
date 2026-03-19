ff786111a8af7c21b74146f58a6b2ccdb0226581
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2026-03-19 19:58:04 +0100

    Revert "Use pino/browser for CF Workers via Vite SSR alias"
    This reverts commit aaada364e183f1bd0b82dd8ebfcb662a06df8308.

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,6 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
-  environments: {
-    ssr: {
-      resolve: {
-        alias: {
-          pino: "pino/browser",
-        },
-      },
-    },
-  },
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
