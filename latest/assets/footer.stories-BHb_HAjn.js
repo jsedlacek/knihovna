023ed9f33b17730c327dda1286922448b92ef565
jsedlacek <jsedlacek@users.noreply.github.com>
2026-03-26 21:31:48 +0000

    deploy: e4d851bf1504ecc7534d428a0562b2db3be8d5f9

--- latest/assets/footer.stories-BHb_HAjn.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./footer-DpEF7PQA.js";var r,i,a,o;e((()=>{t(),r={title:`UI/Footer`,component:n,parameters:{layout:`fullscreen`},tags:[`autodocs`]},i={args:{}},a={args:{lastUpdated:`23. března 2026`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {
-    lastUpdated: "23. března 2026"
-  }
-}`,...a.parameters?.docs?.source}}},o=[`Default`,`WithLastUpdated`]}))();export{i as Default,a as WithLastUpdated,o as __namedExportsOrder,r as default};
