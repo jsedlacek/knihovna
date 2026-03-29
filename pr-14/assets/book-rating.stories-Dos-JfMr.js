fdb58f3765d65d592549d8fe969046c408b898fb
jsedlacek <jsedlacek@users.noreply.github.com>
2026-03-29 13:03:47 +0000

    deploy: 118961b18e3b52e9a72a7e069f6d248bc7c89407

---
+++ pr-14/assets/book-rating.stories-Dos-JfMr.js
@@ -0,0 +1,27 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./book-rating-kIdz8Mgy.js";var r,i,a,o,s,c,l;e((()=>{t(),r={title:`Components/BookRating`,component:n,parameters:{layout:`centered`},tags:[`autodocs`]},i={args:{rating:4.5,ratingsCount:987,url:`https://goodreads.com/book/show/123`}},a={args:{rating:4.2,ratingsCount:1500}},o={args:{rating:3.8}},s={args:{rating:null}},c={args:{rating:4.5,ratingsCount:3200,url:`https://goodreads.com/book/show/123`,size:`base`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    rating: 4.5,
+    ratingsCount: 987,
+    url: "https://goodreads.com/book/show/123"
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    rating: 4.2,
+    ratingsCount: 1500
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    rating: 3.8
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    rating: null
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    rating: 4.5,
+    ratingsCount: 3200,
+    url: "https://goodreads.com/book/show/123",
+    size: "base"
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Default`,`WithoutLink`,`WithoutCount`,`NoRating`,`SizeBase`]}))();export{i as Default,s as NoRating,c as SizeBase,o as WithoutCount,a as WithoutLink,l as __namedExportsOrder,r as default};
