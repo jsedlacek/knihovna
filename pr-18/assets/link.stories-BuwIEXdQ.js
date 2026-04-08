8012dba5cece18157982b780f7ff98f2361ae3d0
jsedlacek <jsedlacek@users.noreply.github.com>
2026-04-08 19:20:36 +0000

    deploy: 3b6bfb6f53b4202c90d244f2d179c1025aeb8369

---
+++ pr-18/assets/link.stories-BuwIEXdQ.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./link-zUWfRJ9s.js";var r,i,a;e((()=>{t(),r={title:`UI/Link`,component:n,parameters:{layout:`centered`},tags:[`autodocs`]},i={args:{children:`Karel Čapek – Krakatit`,href:`#`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Karel Čapek – Krakatit",
+    href: "#"
+  }
+}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
